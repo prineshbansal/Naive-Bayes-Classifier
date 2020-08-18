8da753d64b805d81d0fd6315e35f852f6e16cb2d
prineshbansal <bansal.pr@husky.neu.edu>
2020-08-18 15:37:12 -0400

    initial commit

---
+++ Bigram/nbtest_bg.py
@@ -0,0 +1,104 @@
+import sys
+import json
+from math import log
+from operator import itemgetter
+from glob import glob
+
+def generate_log_ratio(dict1,dict2):
+    ratio_list = {}
+    for k,v in dict1.items():
+        ratio_list[k] = log(v) - log(dict2.get(k))
+    return ratio_list
+
+def print_top_20_values(dict):
+    sorted_dict= sorted(dict.items(), key=itemgetter(1), reverse=True)
+    num = 0
+    for items in sorted_dict:
+        num += 1
+        print(items)
+        if num > 20:
+            break
+
+def write_to_file(predictions_file,prediction_dict):
+    with open(predictions_file,'w') as write:
+        write.write('{:>5}\t{:>5}\t{:>2}\n'.format("file-name", "pos-score", "neg-score"))
+        for key, value in prediction_dict.items():
+            sentence_rev = " ".join(reversed(str(key).split('/')))
+            write.write('{:>5}\t{:>5}\t{:>2}\t'.format(sentence_rev.split()[0], value[0], value[1]))
+            write.write("\n")
+
+if __name__ == '__main__':
+    model_file = sys.argv[1]
+    test_directory = sys.argv[2]
+    predictions_file = sys.argv[3]
+
+    #
+    # Reading data from the model file data
+    #
+    with open(model_file,'r') as read:
+        model_data = json.load(read)
+
+    pos_prob = model_data[0]
+    neg_prob = model_data[1]
+
+    #
+    # Calculating and printing the highest log ratios of positive to negative 
+    # and negative to positive weights. 
+    #
+    pos_to_neg_dict = generate_log_ratio(pos_prob,neg_prob)
+    neg_to_pos_dict = generate_log_ratio(neg_prob,pos_prob)
+
+    print("20 terms with the highest ratio of positive to negative weight:")
+    print_top_20_values(pos_to_neg_dict)
+    print()
+    
+    print("20 terms with the highest ratio of negative to positive weight:")
+    print_top_20_values(neg_to_pos_dict)
+    print()
+
+    #
+    # Reading data from test directory
+    #
+    test_data_files=[]
+    test_data_files = glob(test_directory + '/*.txt')
+    
+    #
+    # Classifying the text file in test directory as postive or negative.  
+    #
+    test_data = {}
+    pos_reviews =[]
+    neg_reviews = []
+    prediction_dict = {}
+
+    for i in test_data_files:
+        with open(i,'r') as test_read:
+            test_data[i] = test_read.read()
+
+        cumulative_positive = 0.0
+        cumulative_negative = 0.0
+
+        words = test_data[i].split()
+        bigrams = zip(*[words[k:] for k in range(2)])
+
+        for bigram in bigrams:
+            term = bigram[0] + " " + bigram[1]
+            cumulative_positive += log(pos_prob.get(term,1))
+            cumulative_negative += log(neg_prob.get(term,1))
+
+        prediction_dict[i] = [cumulative_positive,cumulative_negative]
+
+        if cumulative_positive > cumulative_negative:
+            pos_reviews.append(i)
+        else:
+            neg_reviews.append(i)
+
+    #print(len(pos_reviews))
+    #print()
+    #print(len(neg_reviews))
+    #print()
+    #print(len(prediction_dict))
+    
+    #
+    # Writing filename and positive and negative reviews score to file
+    #
+    write_to_file(predictions_file,prediction_dict)
---
+++ Bigram/nbtrain_bg.py
@@ -0,0 +1,112 @@
+import sys
+from glob import glob
+from collections import Counter
+import json
+
+def read_from_file(files):
+	data = []
+	for i in files:
+		with open(i,'r') as read:
+			data.append(read.read())
+	return data
+
+def generate_frequency_dict(data):
+	freq_dict = {}
+	for i in data:
+		words = i.split()
+		bigrams = zip(*[words[j:] for j in range(2)])
+		for bigram in bigrams:
+			term = bigram[0] + " " + bigram[1]
+			freq_dict[term] = freq_dict.get(term,0) + 1
+	return freq_dict
+
+def combine_dict(dict1,dict2):
+	joint_dict =  Counter(dict1) + Counter(dict2)
+	return joint_dict
+
+def generate_final_freq_dict(subset_dict,total_dict):
+	for i in subset_dict.copy().keys():
+		if total_dict[i] < 5:
+			subset_dict.pop(i)
+	return subset_dict		
+
+if __name__ == '__main__':
+	train_dir = sys.argv[1]
+	model_file = sys.argv[2]
+	
+	neg_files = []
+	pos_files = []
+	neg_data = []
+	pos_data = []
+	neg_words = {}
+	pos_words = {}
+
+	#
+	# Reading data from text files in training directory
+	#
+	neg_files = glob(train_dir + '/neg/*.txt')
+	pos_files = glob(train_dir + '/pos/*.txt')
+	neg_data = read_from_file(neg_files)
+	pos_data = read_from_file(pos_files)
+
+	#
+	# Generating frequency dictionary for words in postive and negative 
+	# training texts.
+	#
+	neg_words = generate_frequency_dict(neg_data)
+	pos_words = generate_frequency_dict(pos_data)
+	
+	#
+	# Generating a dictionary for all the words in the collection
+	# Thus, combining the postive & negative words dictionary
+	#
+	collection_dictionary = combine_dict(neg_words,pos_words)
+
+	#
+	# Adjusting postive and negative word dictionaries to fit missing words 
+	# found in collection dictionary
+	#
+	for i in collection_dictionary.keys():
+		neg_words[i] = neg_words.get(i,0)
+		pos_words[i] = pos_words.get(i,0)
+	
+	#
+	# Updating the negwords and poswords dict to remove terms
+	# that occur less than 5 times in collection dictionary
+	#
+	neg_words = generate_final_freq_dict(neg_words,collection_dictionary)
+	pos_words = generate_final_freq_dict(pos_words,collection_dictionary)
+	neg_words_count = sum(neg_words.values())
+	pos_words_count = sum(pos_words.values())
+	collection_dictionary = combine_dict(neg_words,pos_words)
+	total_count = len(collection_dictionary)
+
+	#
+	# Generating negative and positive probabiity dictionaries with 
+	# add-1 or Laplace Smoothing
+	#
+	neg_prob = {}
+	pos_prob = {}
+
+	for i in neg_words.keys():
+		neg_prob[i] = (neg_words[i] + 1)/(neg_words_count + total_count)
+
+	for i in pos_words.keys():
+		pos_prob[i] = (pos_words[i] + 1)/(pos_words_count + total_count)
+
+	final_prob_list = [pos_prob,neg_prob]
+
+	with open(model_file,'w') as temp:
+		json.dump(final_prob_list,temp)
+
+#	with open('`collectionwords.txt','w') as test:
+#		json.dump(collection_dictionary,test)
+#
+#	with open('`neg_words.txt','w') as test:
+#		json.dump(neg_words,test)
+#
+#	with open('`pos_words.txt','w') as test:
+#		json.dump(pos_words,test)
+#
+#	with open('`count.txt','w') as test:
+#		test.write(str(neg_words_count) + '\n\n' + str(pos_words_count))
